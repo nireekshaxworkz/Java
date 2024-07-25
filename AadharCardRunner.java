6ddd31586456b4b238f51f60fb7ed5618522c18d
nireekshaxworkz <nireekshaxworkz@gmail.com>
2024-07-25 15:16:31 +0530

    new file

---
+++ AadharCardRunner.java
@@ -0,0 +1,21 @@
+class AadharCardRunner{
+	public static void main(String[] args){
+		System.out.println("start main in AadharCardRunner");
+		long aadharNumber1=396782950544l;
+		long aadharNumber2=253968548774l;
+		long aadharNumber3=436474520997l;
+		long aadharNumber4=243356759971l;
+		long aadharNumber5=224598543312l;
+		System.out.println("aadharNumber1");
+	    System.out.println("aadharNumber2");
+		System.out.println("aadharNumber3");
+		System.out.println("aadharNumber4");
+		System.out.println("aadharNumber5");
+
+	long[] aadharNumbers={aadharNumber1,aadharNumber2,aadharNumber3,aadharNumber4,aadharNumber5};
+		System.out.println(aadharNumbers[0]);
+;		int element=aadharNumbers.length;
+		System.out.println(element);
+		System.out.println("end main in AadharCardRunner");
+	}
+}
